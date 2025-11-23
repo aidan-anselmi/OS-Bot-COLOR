019edbe266eee40ae5359355eb7cd02143ba1280
Aidan Anselmi <anselmi2011@gmail.com>
2025-11-23 15:25:25 -0800

    mlm

--- src/model/osrs/mlm.py
+++ src/model/osrs/mlm.py
@@ -27,9 +27,6 @@ class MLM(OSRSBot):
 
         self.break_length_multiplier = random.uniform(.5, 1.5)
         self.break_chance_multiplier = random.uniform(.5, 1.5)
-        
-        self.tree_color = clr.PINK
-        self.bank_color = clr.BLUE
 
     def create_options(self):
         return
@@ -62,6 +59,7 @@ class MLM(OSRSBot):
         self.log_msg("Selecting inventory...")
         self.mouse.move_to(self.win.cp_tabs[3].random_point())
         self.mouse.click()
+        self.scrape()
 
         self.desposit_all_img = imsearch.BOT_IMAGES.joinpath("bank", "deposit_inventory.png")
         self.close_bank_img = imsearch.BOT_IMAGES.joinpath("bank", "close_bank.png")
