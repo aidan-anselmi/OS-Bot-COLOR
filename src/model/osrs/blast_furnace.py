7174b2de5be02c7ecc544a14fe6fa87a354e2d66
Aidan Anselmi <anselmi2011@gmail.com>
2025-11-29 11:10:27 -0800

    asdf

--- src/model/osrs/blast_furnace.py
+++ src/model/osrs/blast_furnace.py
@@ -186,7 +186,7 @@ class BlastFurnace(OSRSBot):
             if math.dist(prev_center, cur_center) < 3:
                 return True
             else:
-                self.logout(f"Player is still moving, {cur_center} vs {prev_center}")
+                self.log_msg(f"Player is still moving, {cur_center} vs {prev_center}")
                 time.sleep(0.2)
 
         self.errors += 1
